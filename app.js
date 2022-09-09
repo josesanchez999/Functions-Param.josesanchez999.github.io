56c40f5cbcabfcca0980c35212d5ecbef71c41a9
josesanchez999 <josejsanchez22@icloud.com>
2022-09-09 13:39:05 -0500

    Changes

---
+++ app.js
@@ -0,0 +1,26 @@
+const name = "Functions & Parameters"
+
+function toUpper(text){
+    const upperCased = text.toUpperCase();
+    console.log(upperCased);
+}
+
+toUpper(name);
+
+let obj = {
+    name: 'Jose',
+    age: 20,
+    education: 'Digital Media'
+  };
+  
+  console.log(obj)
+  
+  obj.college = 'NVC';
+  console.log(obj);
+
+  let btn = document.createElement("button");
+btn.innerHTML = "Click Me";
+btn.onclick = function () {
+  alert("Welcome!");
+};
+document.body.appendChild(btn);
